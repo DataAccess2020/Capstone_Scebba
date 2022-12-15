5e63496ab8f2bea202acb146f08e7fd37c1587d9
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-15 10:46:36 +0100

    list right politicians

---
+++ scripts/02_list_politicians.R
@@ -0,0 +1,13 @@
+auth_as("aut")
+
+right_pol = lists_members(
+  list_id = "1603313961453162498",
+  slug = NULL, 
+  owner_user = NULL,
+  n = 3,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
+)
