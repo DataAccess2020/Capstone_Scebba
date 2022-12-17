9c2e39f76cf5a388622e9ad7825500c98f6b3948
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-17 11:08:34 +0100

    list of politicians

--- scripts/02_list_politicians.R
+++ scripts/02_list_politicians.R
@@ -1,10 +1,44 @@
-auth_as("aut")
-
- lists_members(
+right_pol = lists_members(
   list_id = "1603313961453162498",
   slug = NULL, 
   owner_user = NULL,
-  n = 1,
+  n = 3,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+left_pol = lists_members(
+  list_id = "1603314511724924928",
+  slug = NULL, 
+  owner_user = NULL,
+  n = 3,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+M5S = lists_members(
+  list_id = "1603314947794079745",
+  slug = NULL, 
+  owner_user = NULL,
+  n = 2,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+terzo_polo = lists_members(
+  list_id = "1603315192292687874",
+  slug = NULL, 
+  owner_user = NULL,
+  n = 2,
   cursor = "-1",
   token = NULL,
   retryonratelimit = TRUE,
